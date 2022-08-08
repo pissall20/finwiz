5f4bd24efb19cb22ed18deba3fb3dc9924ce0424
Siddhesh Pisal <pisalsiddhesh@gmail.com>
2022-08-09 02:49:19 +0530

    heroku: :

--- finwiz/settings.py
+++ finwiz/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,6 +84,8 @@ DATABASES = {
     }
 }
 
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -107,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
@@ -122,6 +126,8 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_DIR = BASE_DIR / 'media'
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = 'media/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -140,3 +146,6 @@ EMAIL_HOST_USER = 'fastaxhelp@gmail.com'
 DEFAULT_FROM_EMAIL = 'fastaxhelp@gmail.com'
 EMAIL_HOST_PASSWORD = 'thnyrtctgaccnvxe'
 """
+
+# Configure Django App for Heroku.
+django_heroku.settings(locals())

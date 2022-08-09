2eefc76e47701d82788770e841cd0847f7091893
Siddhesh Pisal <pisalsiddhesh@gmail.com>
2022-08-09 05:38:33 +0530

    Created models

--- website/models.py
+++ website/models.py
@@ -11,3 +11,36 @@ class Contact(models.Model):
 
     def __str__(self):
         return self.email
+
+
+class Client(models.Model):
+    name = models.CharField(max_length=100)
+    logo = models.ImageField(upload_to="client_logos/", blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class FAQ(models.Model):
+    question = models.CharField(max_length=200)
+    answer = models.TextField(max_length=2000)
+
+    def __str__(self):
+        return self.question
+
+
+CLIENT_TYPE = (
+    ("Corporate", "Corporate"),
+    ("Retail", "Retail")
+)
+
+
+class PortFolio(models.Model):
+    name = models.CharField(max_length=100)
+    photo = models.ImageField(upload_to="client_logos/", blank=True, null=True)
+    video_link = models.CharField(max_length=200)
+    client_type = models.CharField(max_length=20, choices=CLIENT_TYPE)
+
+    def __str__(self):
+        return self.name
+
